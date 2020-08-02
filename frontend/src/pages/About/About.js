5e188eeb22e365a1eb9309b60b4202bd7832553a
japerry911 <jack.e.perry@outlook.com>
2020-08-02 09:52:33 -0600

    Added basic routes and integrated react-router-dom

---
+++ frontend/src/pages/About/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { useStyles } from "./AboutStyles";
+
+const About = () => {
+  const styles = useStyles();
+
+  return (
+    <div>
+      <h1>About</h1>
+    </div>
+  );
+};
+
+export default About;
