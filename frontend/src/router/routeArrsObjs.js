d25b137e037bf912b7d08d7e0f5cd484e423c58d
japerry911 <jack.e.perry@outlook.com>
2020-08-02 10:30:06 -0600

    Icons added to Drawer menu items

--- frontend/src/router/routeArrsObjs.js
+++ frontend/src/router/routeArrsObjs.js
@@ -1,8 +1,13 @@
+import HomeIcon from "@material-ui/icons/HomeOutlined";
+import InfoIcon from "@material-ui/icons/InfoOutlined";
+import PhoneIcon from "@material-ui/icons/PhoneOutlined";
+import KeyIcon from "@material-ui/icons/VpnKeyOutlined";
+
 export const NONAUTH_ROUTES_ARRAY = [
-  { path: "/", title: "Home", icon: null },
-  { path: "/about", title: "About", icon: null },
-  { path: "/contact", title: "Contact", icon: null },
-  { path: "/sign-in", title: "Sign In", icon: null },
+  { path: "/", title: "Home", icon: HomeIcon },
+  { path: "/about", title: "About", icon: InfoIcon },
+  { path: "/contact", title: "Contact", icon: PhoneIcon },
+  { path: "/sign-in", title: "Sign In", icon: KeyIcon },
 ];
 
 export const ROUTES_OBJECT = {
