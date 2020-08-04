0a51c1b7f3856079af8da65a6ce3468db338319a
japerry911 <jack.e.perry@outlook.com>
2020-08-04 14:23:14 -0600

    frontend outline finished

--- frontend/src/pages/Upload/Upload.js
+++ frontend/src/pages/Upload/Upload.js
@@ -1,13 +1,108 @@
-import React from "react";
+import React, { useState } from "react";
+import LoadingOverlay from "react-loading-overlay";
+import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import Divider from "@material-ui/core/Divider";
+import Button from "@material-ui/core/Button";
 import { useStyles } from "./UploadStyles";
 
 const Upload = () => {
   const classes = useStyles();
 
+  const [isLoading, setIsLoading] = useState(false);
+
   return (
-    <div>
-      <Typography variant="h3">Upload</Typography>
+    <div className={classes.mainDivStyle}>
+      <LoadingOverlay
+        active={isLoading}
+        spinner
+        text="Loading..."
+        styles={{
+          wrapper: (base) => ({
+            ...base,
+            height: "100%",
+            width: "100%",
+          }),
+        }}
+      >
+        <Grid
+          container
+          className={classes.mainGridStyle}
+          justify="center"
+          alignItems="center"
+        >
+          <Paper elevation={5} className={classes.mainPaperStyle}>
+            <Grid
+              item
+              xs={12}
+              sm={12}
+              md={12}
+              lg={12}
+              xl={12}
+              className={classes.gridItemStyle}
+              align="center"
+            >
+              <Typography variant="h2" className={classes.titleStyle}>
+                UPLOAD
+              </Typography>
+              <Divider className={classes.dividerStyle} />
+            </Grid>
+            <Grid
+              item
+              xs={12}
+              sm={12}
+              md={12}
+              lg={12}
+              xl={12}
+              className={classes.gridItemStyle}
+            >
+              <Paper className={classes.subPaperStyle}>
+                <Typography variant="body1" className={classes.textStyle}>
+                  You may upload a TimeSheet file here. The file must{" "}
+                  <strong>only</strong> be 2 columns with data{" "}
+                  <strong>only</strong> in columns A and B: A.) Clock In, and
+                  B.) Clock Out. Do <strong>not</strong> include the column
+                  headers/titles, only the Clock In/Out data.
+                </Typography>
+              </Paper>
+            </Grid>
+            <Grid
+              item
+              xs={12}
+              sm={12}
+              md={12}
+              lg={12}
+              xl={12}
+              className={classes.gridItemStyle}
+              align="center"
+            >
+              <Paper className={classes.subPaperCenteredStyle}>
+                <Button className={classes.buttonStyle} component="label">
+                  Upload Clocking Sheet
+                  <input type="file" className={classes.fileInputStyle} />
+                </Button>
+              </Paper>
+            </Grid>
+            <Grid
+              item
+              xs={12}
+              sm={12}
+              md={12}
+              lg={12}
+              xl={12}
+              className={classes.gridItemStyle}
+              align="center"
+            >
+              <Paper className={classes.subPaperCenteredStyle}>
+                <Button className={classes.buttonStyle}>
+                  Process Timesheet
+                </Button>
+              </Paper>
+            </Grid>
+          </Paper>
+        </Grid>
+      </LoadingOverlay>
     </div>
   );
 };
--- frontend/src/pages/Upload/UploadStyles.js
+++ frontend/src/pages/Upload/UploadStyles.js
@@ -1,3 +1,107 @@
 import makeStyles from "@material-ui/core/styles/makeStyles";
 
-export const useStyles = makeStyles((theme) => ({}));
+export const useStyles = makeStyles((theme) => ({
+  mainDivStyle: {
+    width: "100%",
+    height: "100%",
+    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), 
+                    url('https://skylord-timekeeping492193924349324.s3.us-east-2.amazonaws.com/Upload/new-data-services-bbXaYbKWnjw-unsplash.jpg')`,
+    backgroundRepeat: "no-repeat",
+    backgroundAttachment: "fixed",
+    backgroundSize: "cover",
+    paddingBottom: "3rem",
+  },
+  mainGridStyle: {
+    height: "100%",
+    width: "100%",
+  },
+  mainPaperStyle: {
+    backgroundColor: theme.palette.primary.main,
+    width: "35%",
+    height: "70%",
+    borderRadius: 12,
+    padding: "2rem",
+    border: `3pt solid ${theme.palette.secondary.main}`,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    alignItems: "center",
+    [theme.breakpoints.only("md")]: {
+      width: "60%",
+    },
+    [theme.breakpoints.only("sm")]: {
+      width: "70%",
+    },
+    [theme.breakpoints.only("xs")]: {
+      width: "72%",
+    },
+  },
+  titleStyle: {
+    fontWeight: "bold",
+    color: theme.palette.secondary.main,
+  },
+  dividerStyle: {
+    backgroundColor: theme.palette.secondary.main,
+    width: "95%",
+  },
+  gridItemStyle: {
+    height: "100%",
+    width: "100%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
+  },
+  subPaperCenteredStyle: {
+    width: "90%",
+    height: "40%",
+    padding: "1rem",
+    backgroundColor: theme.colors.lightGray,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 12,
+    [theme.breakpoints.only("xs")]: {
+      width: "100%",
+      padding: "1rem 0",
+    },
+  },
+  buttonStyle: {
+    backgroundColor: theme.palette.primary.main,
+    border: `1pt solid ${theme.palette.secondary.main}`,
+    color: theme.palette.secondary.main,
+    width: "90%",
+    borderRadius: 12,
+    padding: "1rem",
+    "&:hover": {
+      backgroundColor: theme.palette.secondary.main,
+      border: `1pt solid ${theme.palette.primary.main}`,
+      color: theme.palette.primary.main,
+      transition: "220ms ease-in-out",
+    },
+    [theme.breakpoints.only("sm")]: {
+      width: "90%",
+    },
+    [theme.breakpoints.only("xs")]: {
+      width: "90%",
+      fontSize: ".6rem",
+    },
+  },
+  fileInputStyle: {
+    display: "none",
+  },
+  subPaperStyle: {
+    textIndent: 50,
+    width: "90%",
+    height: "40%",
+    padding: "1rem",
+    backgroundColor: theme.colors.lightGray,
+    display: "flex",
+    alignItems: "center",
+    borderRadius: 12,
+    [theme.breakpoints.only("xs")]: {
+      width: "100%",
+      padding: "1rem 0",
+    },
+  },
+}));
