0339bfeafe0d08e4a9967dbd2770adefcb43334b
japerry911 <jack.e.perry@outlook.com>
2020-08-05 12:52:29 -0600

    bug fixes

--- frontend/src/pages/History/History.js
+++ frontend/src/pages/History/History.js
@@ -1,13 +1,13 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, Fragment } from "react";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
-import HeroHeader from "../../components/HeroHeader/HeroHeader";
 import Divider from "@material-ui/core/Divider";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import Button from "@material-ui/core/Button";
 import goServer from "../../api/goServer";
+import LoadingOverlay from "react-loading-overlay";
 import { useSelector, useDispatch } from "react-redux";
 import { handleOpen } from "../../redux/actions/snackbarActions";
 import { CSVLink } from "react-csv";
@@ -23,7 +23,7 @@ const History = () => {
   const csvBtnRef = useRef(null);
 
   const dispatch = useDispatch();
-  const userID = useSelector((state) => state.users.user.userID);
+  const userID = useSelector((state) => state.users.user.ID);
 
   const dateChange = (dates) => {
     const [start, end] = dates;
@@ -38,7 +38,7 @@ const History = () => {
 
     goServer
       .get(
-        `/api/clockings/1?startDate=${startDate
+        `/api/clockings/${userID}?startDate=${startDate
           .toISOString()
           .replace(
             /T.+Z$/,
@@ -59,111 +59,133 @@ const History = () => {
           csvData.unshift(headers);
 
           setClockingsData(csvData);
-
           csvBtnRef.current.link.click();
+          setIsLoading(false);
         },
-        (error) => console.log(error)
+        () => {
+          dispatch(
+            handleOpen({
+              type: "error",
+              message:
+                "Export Failed. Please refresh the page and try again in a few seconds",
+            })
+          );
+        }
       );
   };
 
   return (
-    <div className={classes.mainDivStyle}>
-      <Grid container justify="center" className={classes.mainContainerStyle}>
-        <Grid item xs={12} sm={12} md={12} lg={12} xl={12} align="center">
-          <Paper className={classes.paperStyle}>
-            <Typography variant="h2" className={classes.titleStyle}>
-              Your Historical Data
-            </Typography>
-            <Divider className={classes.dividerStyle} />
-            <figure className={classes.logoFigureStyle}>
-              <img
-                alt="Skylord's Timekeeping Logo"
-                src="https://skylord-timekeeping492193924349324.s3.us-east-2.amazonaws.com/Logos/logo_transparent_background.png"
-                className={classes.logoImgStyle}
-              />
-            </figure>
-            <Grid
-              container
-              item
-              xs={12}
-              sm={12}
-              md={12}
-              lg={12}
-              xl={12}
-              align="center"
-              justify="space-between"
-              direction="column"
-            >
-              <form className={classes.formStyle} onSubmit={onSubmit}>
-                <DatePicker
-                  selected={startDate}
-                  onChange={dateChange}
-                  startDate={startDate}
-                  endDate={endDate}
-                  filterDate={(date) => date <= new Date()}
-                  selectsRange
-                  inline
-                  disabledKeyboardNavigation
-                />
+    <Fragment>
+      <LoadingOverlay active={isLoading} spinner text="Exporting Data...">
+        <div className={classes.mainDivStyle}>
+          <Grid
+            container
+            justify="center"
+            className={classes.mainContainerStyle}
+          >
+            <Grid item xs={12} sm={12} md={12} lg={12} xl={12} align="center">
+              <Paper className={classes.paperStyle}>
+                <Typography variant="h2" className={classes.titleStyle}>
+                  Your Historical Data
+                </Typography>
+                <Divider className={classes.dividerStyle} />
+                <figure className={classes.logoFigureStyle}>
+                  <img
+                    alt="Skylord's Timekeeping Logo"
+                    src="https://skylord-timekeeping492193924349324.s3.us-east-2.amazonaws.com/Logos/logo_transparent_background.png"
+                    className={classes.logoImgStyle}
+                  />
+                </figure>
                 <Grid
                   container
-                  alignItems="center"
-                  justify="center"
+                  item
+                  xs={12}
+                  sm={12}
+                  md={12}
+                  lg={12}
+                  xl={12}
+                  align="center"
+                  justify="space-between"
                   direction="column"
                 >
-                  <Paper className={classes.subPaperStyle}>
+                  <form className={classes.formStyle} onSubmit={onSubmit}>
+                    <DatePicker
+                      selected={startDate}
+                      onChange={dateChange}
+                      startDate={startDate}
+                      endDate={endDate}
+                      filterDate={(date) => date <= new Date()}
+                      selectsRange
+                      inline
+                      disabledKeyboardNavigation
+                    />
                     <Grid
                       container
-                      item
-                      xs={12}
-                      sm={12}
-                      md={12}
-                      lg={12}
-                      xl={12}
                       alignItems="center"
-                      justify="space-between"
+                      justify="center"
+                      direction="column"
                     >
-                      <Typography variant="body1" className={classes.textStyle}>
-                        <strong>Start Date:</strong>{" "}
-                        {startDate ? startDate.toDateString() : null}
-                      </Typography>
-                      <Typography variant="body1" className={classes.textStyle}>
-                        <strong>End Date:</strong>{" "}
-                        {endDate ? endDate.toDateString() : null}
-                      </Typography>
+                      <Paper className={classes.subPaperStyle}>
+                        <Grid
+                          container
+                          item
+                          xs={12}
+                          sm={12}
+                          md={12}
+                          lg={12}
+                          xl={12}
+                          alignItems="center"
+                          justify="space-between"
+                        >
+                          <Typography
+                            variant="body1"
+                            className={classes.textStyle}
+                          >
+                            <strong>Start Date:</strong>{" "}
+                            {startDate ? startDate.toDateString() : null}
+                          </Typography>
+                          <Typography
+                            variant="body1"
+                            className={classes.textStyle}
+                          >
+                            <strong>End Date:</strong>{" "}
+                            {endDate ? endDate.toDateString() : null}
+                          </Typography>
+                        </Grid>
+                      </Paper>
+                      <Grid
+                        item
+                        xs={12}
+                        sm={12}
+                        md={12}
+                        lg={12}
+                        xl={12}
+                        className={classes.gridItemStyle}
+                      >
+                        <Button
+                          className={classes.buttonStyle}
+                          type="submit"
+                          disabled={startDate === null || endDate === null}
+                        >
+                          Export Data
+                        </Button>
+                        <CSVLink
+                          data={clockingsData}
+                          filename="test.csv"
+                          className={classes.hiddenStyle}
+                          ref={csvBtnRef}
+                          target="_blank"
+                        />
+                      </Grid>
                     </Grid>
-                  </Paper>
-                  <Grid
-                    item
-                    xs={12}
-                    sm={12}
-                    md={12}
-                    lg={12}
-                    xl={12}
-                    className={classes.gridItemStyle}
-                  >
-                    <Button
-                      className={classes.buttonStyle}
-                      type="submit"
-                      disabled={startDate === null || endDate === null}
-                    >
-                      Export Data
-                    </Button>
-                    <CSVLink
-                      data={clockingsData}
-                      filename="test.csv"
-                      className={classes.hiddenStyle}
-                      ref={csvBtnRef}
-                      target="_blank"
-                    />
-                  </Grid>
+                  </form>
                 </Grid>
-              </form>
+              </Paper>
             </Grid>
-          </Paper>
-        </Grid>
-      </Grid>
-    </div>
+          </Grid>
+        </div>
+      </LoadingOverlay>
+    </Fragment>
   );
 };
 
