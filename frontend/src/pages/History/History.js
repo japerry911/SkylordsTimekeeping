52e50631acee27cf261b753a64f29a47f86e583f
japerry911 <jack.e.perry@outlook.com>
2020-08-05 10:07:31 -0600

    Outline of history page complete

--- frontend/src/pages/History/History.js
+++ frontend/src/pages/History/History.js
@@ -1,13 +1,120 @@
-import React from "react";
+import React, { useState } from "react";
 import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import HeroHeader from "../../components/HeroHeader/HeroHeader";
+import Divider from "@material-ui/core/Divider";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import Button from "@material-ui/core/Button";
 import { useStyles } from "./HistoryStyles";
 
 const History = () => {
   const classes = useStyles();
 
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+
+  const dateChange = (dates) => {
+    const [start, end] = dates;
+    setStartDate(start);
+    setEndDate(end);
+  };
+
   return (
-    <div>
-      <Typography variant="h3">History</Typography>
+    <div className={classes.mainDivStyle}>
+      <HeroHeader
+        headerText="HISTORICAL DATA"
+        imgUrl="https://skylord-timekeeping492193924349324.s3.us-east-2.amazonaws.com/History/pexels-lukas-590022.jpg"
+      />
+      <Grid container justify="center" className={classes.mainContainerStyle}>
+        <Grid item xs={12} sm={12} md={12} lg={12} xl={12} align="center">
+          <Paper className={classes.paperStyle}>
+            <Typography variant="h2" className={classes.titleStyle}>
+              Your Historical Data
+            </Typography>
+            <Divider className={classes.dividerStyle} />
+            <figure className={classes.logoFigureStyle}>
+              <img
+                alt="Skylord's Timekeeping Logo"
+                src="https://skylord-timekeeping492193924349324.s3.us-east-2.amazonaws.com/Logos/logo_transparent_background.png"
+                className={classes.logoImgStyle}
+              />
+            </figure>
+            <Grid
+              container
+              item
+              xs={12}
+              sm={12}
+              md={12}
+              lg={12}
+              xl={12}
+              align="center"
+              justify="space-between"
+              direction="column"
+            >
+              <form className={classes.formStyle}>
+                <DatePicker
+                  selected={false}
+                  onChange={dateChange}
+                  startDate={startDate}
+                  endDate={endDate}
+                  filterDate={(date) => date <= new Date()}
+                  selectsRange
+                  inline
+                  disabledKeyboardNavigation
+                />
+                <Grid
+                  container
+                  alignItems="center"
+                  justify="center"
+                  direction="column"
+                >
+                  <Paper className={classes.subPaperStyle}>
+                    <Grid
+                      container
+                      item
+                      xs={12}
+                      sm={12}
+                      md={12}
+                      lg={12}
+                      xl={12}
+                      alignItems="center"
+                      justify="space-between"
+                    >
+                      <Typography variant="body1" className={classes.textStyle}>
+                        <strong>Start Date:</strong>{" "}
+                        {startDate ? startDate.toDateString() : null}
+                      </Typography>
+                      <Typography variant="body1" className={classes.textStyle}>
+                        <strong>End Date:</strong>{" "}
+                        {endDate ? endDate.toDateString() : null}
+                      </Typography>
+                    </Grid>
+                  </Paper>
+                  <Grid
+                    item
+                    xs={12}
+                    sm={12}
+                    md={12}
+                    lg={12}
+                    xl={12}
+                    className={classes.gridItemStyle}
+                  >
+                    <Button
+                      className={classes.buttonStyle}
+                      type="submit"
+                      disabled={startDate === null || endDate === null}
+                    >
+                      Export Data
+                    </Button>
+                  </Grid>
+                </Grid>
+              </form>
+            </Grid>
+          </Paper>
+        </Grid>
+      </Grid>
     </div>
   );
 };
--- frontend/src/pages/History/HistoryStyles.js
+++ frontend/src/pages/History/HistoryStyles.js
@@ -1,3 +1,108 @@
 import makeStyles from "@material-ui/core/styles/makeStyles";
 
-export const useStyles = makeStyles((theme) => ({}));
+export const useStyles = makeStyles((theme) => ({
+  mainDivStyle: {
+    minHeight: "95rem",
+    width: "100%",
+    backgroundColor: theme.palette.secondary.main,
+    paddingBottom: "3rem",
+    [theme.breakpoints.only("xs")]: {
+      minHeight: "70rem",
+    },
+  },
+  paperStyle: {
+    padding: "1rem 0",
+    marginTop: "3rem",
+    height: "100%",
+    width: "47%",
+    backgroundColor: theme.palette.primary.main,
+    borderRadius: 12,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "space-between",
+    [theme.breakpoints.only("md")]: {
+      height: "100%",
+      width: "80%",
+    },
+    [theme.breakpoints.only("sm")]: {
+      height: "100%",
+      width: "90%",
+    },
+    [theme.breakpoints.only("xs")]: {
+      height: "88%",
+      width: "92%",
+    },
+  },
+  titleStyle: {
+    fontWeight: "bold",
+    color: theme.palette.secondary.main,
+  },
+  dividerStyle: {
+    backgroundColor: theme.palette.secondary.main,
+    width: "95%",
+  },
+  mainContainerStyle: {
+    minHeight: "50rem",
+  },
+  logoImgStyle: {
+    width: "40%",
+    height: "auto",
+    [theme.breakpoints.only("xs")]: {
+      width: "100%",
+    },
+  },
+  subPaperStyle: {
+    width: "70%",
+    height: "90%",
+    padding: "1rem 0",
+    marginTop: "3rem",
+    backgroundColor: theme.colors.lightGray,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "space-between",
+    borderRadius: 12,
+    [theme.breakpoints.only("xs")]: {
+      width: "88%",
+      padding: "0.5rem 0",
+    },
+  },
+  formStyle: {
+    width: "100%",
+  },
+  textStyle: {
+    color: theme.palette.primary.main,
+    padding: "1rem 3rem",
+    fontSize: "1.1rem",
+    [theme.breakpoints.only("xs")]: {
+      fontSize: ".9rem",
+    },
+  },
+  buttonStyle: {
+    backgroundColor: theme.palette.primary.main,
+    border: `2pt solid ${theme.palette.secondary.main}`,
+    color: theme.palette.secondary.main,
+    width: "70%",
+    borderRadius: 12,
+    padding: "1.25rem",
+    "&:hover": {
+      backgroundColor: theme.palette.secondary.main,
+      border: `2pt solid ${theme.palette.primary.main}`,
+      color: theme.palette.primary.main,
+      transition: "220ms ease-in-out",
+    },
+    [theme.breakpoints.only("sm")]: {
+      width: "90%",
+    },
+    [theme.breakpoints.only("xs")]: {
+      width: "90%",
+      fontSize: ".6rem",
+    },
+  },
+  gridItemStyle: {
+    marginTop: "3rem",
+    height: "100%",
+    width: "100%",
+  },
+}));
